e6808724300c13b5ae9921f426c850fdf5448434
AlvarOSierra1998 <alvaro-olmos12@hotmail.com>
2026-03-12 15:05:23 +0100

    organizacion del proyecto

---
+++ src/views/InicioView.vue
@@ -0,0 +1,13 @@
+
+<script setup lang = ts>
+// Aquí irá la lógica de tu inicio
+
+</script>
+
+<template>
+  <div>
+    <h1>Bienvenido a mi web de recetas</h1>
+    <p>Próximamente: las mejores recetas del mundo</p>
+  </div>
+</template>
+
